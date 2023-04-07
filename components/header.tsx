6ac6007536e989334b0d4f4403e0bf761912c60d
Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>
2023-04-07 13:32:53 -0700

    got navbar to stay at top

--- components/header.tsx
+++ components/header.tsx
@@ -22,41 +22,36 @@ const navItems = {
 export default function Header() {
   let pathname = usePathname() || '/';
   return (
-    <div className={styles.navbar}>
-      <nav
-        className=""
-        id="nav"
-      >
-        <div className={styles.mainheader}>
-          <Link href='/'>
-            <p className='text-xl font-medium'>
-              Antonio Jesus Ayala
-              {/* <span className='text-l'> and </span>
+    <nav className={styles.navbar}>
+      <div className={styles.mainheader}>
+        <Link href='/'>
+          <p className='text-xl font-medium'>
+            Antonio Jesus Ayala
+            {/* <span className='text-l'> and </span>
             <code className={styles.code}>Software Engineer</code> */}
-            </p>
-          </Link>
-          <div>
-            Software Engineer and Web Developer
-          </div>
-        </div>
+          </p>
+        </Link>
+        <span>
+          Software Engineer and Web Developer
+        </span>
+      </div>
 
-        <div className="">
-          {Object.entries(navItems).map(([path, { name }]) => {
-            const isActive = path === pathname;
-            return (
-              <Link
-                key={path}
-                href={path}
-                className=""
-              >
-                <span className={styles.paths}>
-                  {name}
-                </span>
-              </Link>
-            );
-          })}
-        </div>
-      </nav >
-    </div >
+      <div className="">
+        {Object.entries(navItems).map(([path, { name }]) => {
+          const isActive = path === pathname;
+          return (
+            <Link
+              key={path}
+              href={path}
+              className=""
+            >
+              <span className={styles.paths}>
+                {name}
+              </span>
+            </Link>
+          );
+        })}
+      </div>
+    </nav >
   );
 }
