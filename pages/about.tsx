97550f915c0cac2b377a47d20a5f4a09226060ec
Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>
2023-04-08 14:36:46 -0700

    more overall styling. relocating where pages start

--- pages/about.tsx
+++ pages/about.tsx
@@ -12,25 +12,28 @@ export default function AboutPage() {
   return (
     <section className={styles.main}>
       <h1 className="font-bold text-5xl font-serif">About Me</h1>
-      <br></br>
-      <p className="my-5">
-        My name is Antonio and I enjoy creating things that live on the internet. My interest in web development is driven by curiosity and passion. I thoroughly enjoy a challenge and want the end result to be stimulating.
-        <br></br>
-        <br></br>
-        I’ve had the privilege of working with multiple agencies, allowing me to expand my skill set. From creating full-stack applications and websites to mentoring junior engineers, I’ve loved my time as a software engineer.
-        <br></br>
-        <br></br>
-        My current focus is building accessible, interactive digital experiences for businesses, including a design studio, mailing service, and printing company.
-      </p>
-      <Image
-        alt='Antonio Jesus Ayala'
-        className="rounded-full"
-        src={avatar}
-        placeholder="blur"
-        width={500}
-        priority
-      />
-      <br></br>
-    </section>
+      <div className=''>
+
+        <section className=''>
+          <p className="my-3">
+            My name is Antonio and I enjoy creating things that live on the internet. My interest in web development is driven by curiosity and passion. I thoroughly enjoy a challenge and want the end result to be stimulating.
+          </p>
+          <p className="my-3">
+            I’ve had the privilege of working with multiple agencies, allowing me to expand my skill set. From creating full-stack applications and websites to mentoring junior engineers, I’ve loved my time as a software engineer.
+          </p>
+          <p className="my-3">
+            My current focus is building accessible, interactive digital experiences for businesses, including a design studio, mailing service, and printing company.
+          </p>
+        </section>
+        <Image
+          alt='Antonio Jesus Ayala'
+          className="rounded-full"
+          src={avatar}
+          placeholder="blur"
+          width={500}
+          priority
+        />
+      </div>
+    </section >
   );
 }
